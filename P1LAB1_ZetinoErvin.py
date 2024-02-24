74556634934d13aded116a3a89a285a6ba42fa9e
ervinzetino <160455974+ervinzetino@users.noreply.github.com>
2024-02-23 21:08:19 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_ZetinoErvin.py
@@ -0,0 +1,4 @@
+#Ervn Zetino
+print('February 20, 2024')
+print('P1LAB1')
+print('This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.')
